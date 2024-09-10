b4a2e2179b07368072bd4e82597cb7287f581005
Santiago Finamore <18-10125@usb.ve>
2024-09-10 15:59:38 -0400

    Fixed Colorscheme appliance

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- Set some global variables
 vim.g.mapleader = " "
-vim.cmd.colorscheme = "onedark vivid"
 vim.go.tabstop = 4
 vim.go.softtabstop = 4
 vim.go.shiftwidth = 4
@@ -11,3 +10,6 @@ vim.keymap.set('n', '<leader>e', vim.cmd.Ex, {})
 
 -- Lazy.nvim
 require('config.lazy')
+
+-- Colorscheme
+vim.cmd([[colorscheme onedark]])
