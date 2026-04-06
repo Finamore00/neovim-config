67756f85c0a117e62e2fb6d7f104a3569164fc2a
Santiago Finamore <finamore00@gmail.com>
2026-04-06 10:24:50 -0400

    Add keymaps for git blame and hunk preview in gitsigns

--- plugin/gitsigns.lua
+++ plugin/gitsigns.lua
@@ -1,3 +1,4 @@
 vim.pack.add({ 'https://github.com/lewis6991/gitsigns.nvim' })
 
-
+vim.keymap.set('n', '<leader>gb', '<Cmd>Gitsigns toggle_current_line_blame<CR>', { desc = 'Toggle git blame' })
+vim.keymap.set('n', '<leader>gh', '<Cmd>Gitsigns preview_hunk<CR>', { desc = 'Preview git hunk' })
