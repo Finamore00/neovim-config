1caa8fe324161e1ca32655fd1ac403201d508764
Santiago Finamore <finamore00@gmail.com>
2026-04-12 15:25:53 -0400

    Added missing autopairs configuration for nvim-cmp

--- plugin/nvim-cmp.lua
+++ plugin/nvim-cmp.lua
@@ -10,30 +10,31 @@ vim.pack.add({
 })
 
 local cmp = require('cmp')
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.setup({
-   snippet = {
-     expand = function(args)
-       require('luasnip').lsp_expand(args.body)
-     end,
-   },
-   window = {
-     completion = cmp.config.window.bordered(),
-     documentation = cmp.config.window.bordered(),
-   },
-   mapping = cmp.mapping.preset.insert({
-     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-     ['<C-Space>'] = cmp.mapping.complete(),
-     ['<C-e>'] = cmp.mapping.abort(),
-     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-   }),
-   sources = cmp.config.sources({
-     { name = 'nvim_lsp' },
-     { name = 'luasnip' }, -- For luasnip users.
-   }, {
-     { name = 'buffer' },
-   })
- })
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
+    }, {
+        { name = 'buffer' },
+    })
+})
 
 cmp.setup.cmdline({ '/', '?' }, {
     mapping = cmp.mapping.preset.cmdline(),
@@ -43,11 +44,16 @@ cmp.setup.cmdline({ '/', '?' }, {
 })
 
 cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  }),
-  matching = { disallow_symbol_nonprefix_matching = false }
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
 })
+
+cmp.event:on(
+    'confirm_done',
+    cmp_autopairs.on_confirm_done()
+)
